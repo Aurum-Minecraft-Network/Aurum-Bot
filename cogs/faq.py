d6c52bff606ff1589da280e6df09aba57782b1c8
YouTubeATP <YouTubeATP@users.noreply.github.com>
2021-02-02 20:35:37 +0800

    Add FAQ entry, fix grammar and add index range check for FAQ command

--- cogs/faq.py
+++ cogs/faq.py
@@ -11,13 +11,16 @@ class FAQ(commands.Cog):
         with open("assets/faq.json", "r", encoding="utf-8") as f:
             faqs = json.load(f)
         if not index:
-            msg = "Here are the list of items. Use `a!faq [number]` to get more help.\n"
+            msg = "Here is the list of items. Use `a!faq [number]` to get more help.\n"
             for index, entry in enumerate(faqs):
                 msg += f"""**{entry["name"]}** - `{index + 1}`\n"""
             await ctx.send(msg)
         else:
-            entry = faqs[int(index) - 1]
-            await ctx.send(f"""__**{entry["name"]}**__\n{entry["value"]}""")
+            try:
+                entry = faqs[int(index) - 1]
+                await ctx.send(f"""__**{entry["name"]}**__\n{entry["value"]}""")
+            except IndexError:
+                await ctx.send(f"The number you gave is out of range! Please give a number between `1` and `{len(faqs)}`!")
 
 def setup(bot):
     bot.add_cog(FAQ(bot))
