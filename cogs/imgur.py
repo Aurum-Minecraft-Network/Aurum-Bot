e88ef3df3824acaaf2e6e0b3250ed07952f0e91b
YouTubeATP <YouTubeATP@users.noreply.github.com>
2021-02-19 17:36:44 +0800

    Add auto upload image feature

--- cogs/imgur.py
+++ cogs/imgur.py
@@ -23,6 +23,23 @@ class Imgur(commands.Cog):
                 msg += f"{uploaded_image.link}\n"
                 os.remove(os.path.abspath(f"./{i.filename}")) # Delete image to save space
         await ctx.send(msg)
+        
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content.upper().startswith(("[IOM REQUEST]", "**[IOM REQUEST]**", "_[IOM REQUEST]_", "*[IOM REQUEST]*")):
+            if not message.attachments:
+                await message.channel.send("Invalid request! Please attach at least one image.")
+            elif len(message.attachments) > 1:
+                await message.channel.send("Invalid request! Please attach only one image.")
+            else:
+                if message.attachments[0].filename.endswith((".jpg", ".jpeg", ".png", ".gif")):
+                    await message.attachments[0].save(message.attachments[0].filename) # Download image
+                    im = pyimgur.Imgur(self.imgurid) # Intialize Pyimgur
+                    uploaded_image = im.upload_image(os.path.abspath(f"./{message.attachments[0].filename}"), title="ATP City")
+                    os.remove(os.path.abspath(f"./{message.attachments[0].filename}")) # Delete image to save space
+                    await message.channel.send(f"Automatically uploaded attached image to Imgur. Link:\n{uploaded_image.link}")
+                else:
+                    await message.channel.send("Invalid request! Please attach an image file.")
 
 def setup(bot):
     bot.add_cog(Imgur(bot))
