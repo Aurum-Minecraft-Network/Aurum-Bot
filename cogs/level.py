2e89fa241ef62c9e18733a7c40aaee1b43487e0b
YouTubeATP <YouTubeATP@users.noreply.github.com>
2021-04-02 02:17:46 +0800

    Working sorting algoritm (complicated)

--- cogs/level.py
+++ cogs/level.py
@@ -35,12 +35,12 @@ class Level(commands.Cog):
     async def getXP(self):
         channel = self.bot.get_channel(797745275253817354)
         msg = await channel.fetch_message(826864218686488606)
-        return defaultdict(int, OrderedDict({k: v for k, v in sorted(json.loads(msg.content).items(), key=lambda item: item[-1])}))
+        return defaultdict(int, OrderedDict(reversed(list({k: v for k, v in sorted(json.loads(msg.content).items(), key=lambda item: item[1])}.items()))))
     
     async def updateXP(self, dictionary):
         channel = self.bot.get_channel(797745275253817354)
         msg = await channel.fetch_message(826864218686488606)
-        dictionary = OrderedDict({k: v for k, v in sorted(dictionary.items(), key=lambda item: item[-1])})
+        dictionary = OrderedDict(reversed(list({k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1])}.items())))
         await msg.edit(content=json.dumps(dictionary, indent=4))
         
     async def getDark(self):
