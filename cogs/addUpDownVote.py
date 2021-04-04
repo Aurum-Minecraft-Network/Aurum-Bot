0340036473e29556f9be7ec732586206782ea671
YouTubeATP <YouTubeATP@users.noreply.github.com>
2021-04-04 20:27:45 +0800

    Bot automatically adds upvote and downvote option for messages in #naming-suggestions

---
+++ cogs/addUpDownVote.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class addUpDownVote(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id != 828240711144570911:
+            return
+        await message.add_reaction("<:upvote:828242926643445771>")
+        await message.add_reaction("<:downvote:828242926672281660>")
+    
+def setup(bot):
+    bot.add_cog(addUpDownVote(bot))
