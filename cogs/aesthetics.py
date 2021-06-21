c1a5199e566ad7472efb49e662fb14b2a50c20b5
YouTubeATP <YouTubeATP@users.noreply.github.com>
2021-06-22 00:25:18 +0800

    Fix bug in aesthetics.py

--- cogs/aesthetics.py
+++ cogs/aesthetics.py
@@ -200,7 +200,7 @@ class Aesthetics(commands.Cog):
                 title += "Design Language Updated"
                 embed = discord.Embed(
                     title=title,
-                    description="Updated design language to `simple.`",
+                    description=f"Updated design language to `{arg2}`.",
                     color=int(get_embedColor()[str(ctx.author.id)], 16),
                 )
                 if not get_design()[str(ctx.author.id)]:
